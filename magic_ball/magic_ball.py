a71ecc183a0b310c5b0339195dfdb7ab3406826a
Rebecca Hanlon <ryhanlon@gmail.com>
2017-04-21 12:32:51 -0700

    "A game using random.choice."

---
+++ magic_ball/magic_ball.py
@@ -0,0 +1,61 @@
+import random
+
+predictions = ["Absolutely, baby!",
+               "As I see it, totally!",
+               "Go for it, sugar!",
+               "Without a doubt!",
+               "The signs point to awesomely yes!",
+               "Oh, sugar, sugar, positively yes!",
+               "Quite, indeed it, I concur!",
+               "Honey-pie, you gotta be yes!",
+               "Yes, for a wonderful outcome!",
+               "Sugar-plum, it will be wonderful!",
+               "Oh, no! Don't go there!",
+               "Sugar, read my lips.  No!",
+               "Nada, no, absolutely not",
+               "Sweetheart, that's not the way to go!",
+               "Totally, nada!",
+               "Babes, not sure on that one!",
+               "Sweet-pea, it's totally hazy!",
+               "Not enough data, darling!",
+               "Honey-bun, I better not tell you!",
+               "My cinnamon roll, concentrate and ask again!"]
+
+
+
+def generator():
+    """
+    Use random.choice() for the prediction of Magic 8 ball. Give prediction and ask if they want to play again.
+    :return: 
+    """
+    predictor = random.choice(predictions)
+    print(predictor)
+
+    play_again = input("Do you want to know more about your future? Y/yes or N/no ").lower()
+    if play_again == 'y':
+        input("Type in your question?")
+        generator()
+    else:
+        print("Your future looks marvelous, keep smiling!")
+        quit()
+
+
+def welcome():
+    """
+    Print a welcome screen for the user.
+    :return: 
+    """
+    want_to_play = input('Welcome to your future!  Do you want to know your future?  >>Y/yes or N/no>').lower()
+    while True:
+        if want_to_play == 'y':
+            my_question = input("Type in your question. >>").lower()
+
+            generator()
+
+        else:
+            print("Oh, you hurt my feelings!  Good bye!")
+
+            break
+
+
+welcome()
