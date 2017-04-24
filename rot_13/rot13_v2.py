78bb82b373a5e3d3e029c59ca7266dc54d31c96b
Rebecca Hanlon <ryhanlon@gmail.com>
2017-04-23 23:04:57 -0700

    Looping, puzzle.

--- rot_13/rot13_v2.py
+++ rot_13/rot13_v2.py
@@ -13,24 +13,37 @@ import string
 def decoder():
     abc_forward = string.ascii_lowercase
     abc_swap = string.ascii_lowercase[13:26] + abc_forward[0:13]
+    white_space = string.whitespace
+    punctuation = string.punctuation
 
-    print(abc_forward, abc_swap, sep='\n')
+    #print(abc_forward, abc_swap, sep='\n')
 
     message = "Ohg, bssvpre, V qvqa'g pngpu gurfr -- gurl ner zl crg svfu naq V whfg\
      oevat gurz urer gb fjvz. Jura gurl'er qbar gurl whzc onpx vagb gur ohpxrg.".lower()
 
-    result = ''
+    result = []
     for char in message:
 
-        if char not in abc_swap:
-            result += char  # Punctuation, whitespace
+        # if char in white_space or char in punctuation:
+        #     result += char  # Punctuation, whitespace
 
-        elif char in abc_swap:
-            position = abc_swap.index(char)
-            decripted =
-            result += char
+        if char in abc_swap:
+            code_index = abc_swap.index(char)
 
-    return result
+            result.append(code_index)
+    #print(result, end='')
+
+#     final = str()
+    for char in result:
+        purr = abc_forward[char]
+        #meow = str(purr)
+#         final += purr
+
+        print(purr, end=' ')
+
+
+
+            # print(result)
 
 
 decoder()
