ae1f5fca1d67cfbf18ec6a837226992d9ef7e42e
Rebecca Hanlon <ryhanlon@gmail.com>
2017-04-25 16:57:25 -0700

    "Used List Comp, nested For loops, and itertools/chain.from_iterable to make lists within a list into one list."

--- nest/nest.py
+++ nest/nest.py
@@ -1,6 +1,49 @@
 """
 >>> letters = [['a', 'b', 'c'], ['d', 'e', 'f', 'g'], ['h', 'i']]
 
->>> denest()
+>>> denest(letters)
 ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
 """
+
+from itertools import chain
+
+
+
+# def denest(letters):
+#     """
+#     Used chain.from_iterable() to make into one list.
+#     :param letters: lists within a list
+#     :return: a single list
+#     """
+#
+#     use_iter = list(chain.from_iterable(letters))
+#     return use_iter
+
+
+# def denest(iterable):
+#     """
+#     Used nested for loop to remove make into one list.
+#     :param iterable: lists within a list
+#     :return: result, a single list
+#     """
+#
+#     result = list()
+#     for sub_list in iterable:
+#         for word in sub_list:
+#             result.append(word)
+#
+#     return result
+
+
+def denest(iterable):
+    """
+    Used List Comp. to make into one list.
+    :param iterable: lists within a list
+    :return: result, one list
+    """
+
+    result = [item for sub_list in iterable
+            for item in sub_list]
+
+    return result
+
