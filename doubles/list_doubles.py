c06a9ea2386eb3279abc61fdc7e94d50149de752
Rebecca Hanlon <ryhanlon@gmail.com>
2017-04-26 12:29:48 -0700

    "Used List Comp, nested For loops, and itertools/chain.from_iterable to make lists within a list into one list."

--- doubles/list_doubles.py
+++ doubles/list_doubles.py
@@ -10,3 +10,53 @@
 
 
 # Write your code below
+
+from operator import mul
+
+
+    # version 3
+
+def apply_to_all(list_one, n):
+    """
+    Used List Comp and mul() to get rid of 0 and multiply item in list by 2.
+
+    :param list_one: list of ints
+    :param n: int 
+    :return: int
+    """
+    purr = [mul(item, 2) for item in list_one if item > 0]
+
+    return purr
+
+
+    # version 2
+
+# def apply_to_all(list_one, n):
+#     """
+#     Used List Comp to get rid of 0 and multiply item in list by 2.
+#
+#     :param list_one: list of ints
+#     :param n: int
+#     :return: int
+#     """
+#     purr = [item * 2 for item in list_one if item > 0]
+#
+#     return purr
+
+
+    # version 1
+
+# def apply_to_all(list_one, n):
+#     """
+#     Used 'for' looping to get rid of 0 and multiply item in list by 2.
+#     :param list_one:  list of ints
+#     :param n: int
+#     :return: ints
+#     """
+#     result = list()
+#
+#     for item in list_one:
+#         if item > 0:
+#             result += [item * 2]
+#
+#     return result
