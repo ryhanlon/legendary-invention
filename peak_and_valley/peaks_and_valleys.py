abbf6e2b7d0996970114acc12bc6a0a455a19b81
Rebecca Hanlon <ryhanlon@gmail.com>
2017-04-27 13:39:06 -0700

    "Doctext list slicing, getting index in scope a loop. "

--- peak_and_valley/peaks_and_valleys.py
+++ peak_and_valley/peaks_and_valleys.py
@@ -17,3 +17,73 @@
 [[1, 2, 3, 4, 5, 6, 7], [6, 5, 4], [5, 6, 7, 8, 9], [8, 7, 6], [7, 8, 9]]
 
 """
+
+
+def peaks(data):
+    result = list()
+    for index, num in enumerate(data):
+
+        if index == 0 or index == (len(data) - 1):
+            continue
+
+        num_right = data[index + 1]
+        num_left = data[index - 1]
+
+        if num > num_right and num > num_left:
+            result.append(index)
+
+    return result
+
+
+def valleys(data):
+    result = list()
+    for index, num in enumerate(data):
+
+        if index == 0 or index == (len(data) - 1):
+            continue
+
+        num_right = data[index+1]
+        num_left = data[index-1]
+
+        if num < num_right and num < num_left:
+            result.append(index)
+
+    return result
+
+
+
+def peaks_and_valleys(data):
+    peaks_list = peaks(data)
+    valleys_list = valleys(data)
+
+    return sorted(peaks_list + valleys_list)
+
+
+# def peaks_and_valleys(data):
+        # only with return
+#     return sorted(peaks(data) + valleys(data))
+
+
+
+# def peaks_and_valleys(data):
+#     result = list()
+#     for index, num in enumerate(data):
+#
+#         if index == 0 or index == (len(data) - 1):
+#             continue
+#
+#         num_right = data[index-1]
+#         num_left = data[index+1]
+#
+#         if num > num_right and num > num_left:
+#             result.append(index)
+#
+#         if num < num_right and num < num_left:
+#             result.append(index)
+#
+#     return result
+
+
+
+
+#def chop(data, points_of_interest):
