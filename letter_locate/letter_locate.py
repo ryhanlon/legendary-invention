0053509b5f114beb1f867c84c82c0c94c68f80d2
Rebecca Hanlon <ryhanlon@gmail.com>
2017-04-27 13:40:40 -0700

    "Moved into files"

--- letter_locate/letter_locate.py
+++ letter_locate/letter_locate.py
@@ -9,3 +9,18 @@
 >>> locate('i', 'mississippi')
 [1, 4, 7, 10]
 """
+
+
+def locate(letter, word):
+    result = list()
+    for index, char in enumerate(word):
+        if char == letter:
+            result.append(index)
+
+    return result
+
+
+# def locate(letter, word):
+#
+#     result = [index for index, char in enumerate(word) if char == letter]
+#     return result
