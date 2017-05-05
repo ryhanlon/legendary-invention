769063ad307d6fa19fd63f73e637648511b236ee
Rebecca Hanlon <ryhanlon@gmail.com>
2017-05-05 16:18:57 -0700

    "Made an ARI calculator.  Used dictionaries, RegEx and a module for quantifying the text."

--- labs_Applied_Python/ari/main.py
+++ labs_Applied_Python/ari/main.py
@@ -3,7 +3,7 @@ Creating an ARI (automated readability index) to evaluate the reading level of a
 
 
     # evaluate the text, compute the ARI score
-    #4.71 9(characters/words) +.5 (words/sentences) -21.43
+    #4.71 9(characters/words) +.(5 (words/sentences) -21.43)
 
     # ari_scale = {...}, this is used to apply the score to the age and grade
 
@@ -14,9 +14,66 @@ import os
 from collections import defaultdict
 from collections import Counter
 import chalk
+import math
+from quantify_text import sents_counter
+from quantify_text import word_counter
+from quantify_text import char_counter
 
 
 BOOKS = '/Users/MacBookPro/Git/Projects_FSP/labs_Applied_Python/ari/books/'
+
+
+def score_ari(score: int, text_file: str) -> None:
+    ari_scale = {
+        1: {'ages': '5-6', 'grade_level': 'Kindergarten'},
+        2: {'ages': '6-7', 'grade_level': '1st Grade'},
+        3: {'ages': '7-8', 'grade_level': '2nd Grade'},
+        4: {'ages': '8-9', 'grade_level': '3rd Grade'},
+        5: {'ages': '9-10', 'grade_level': '4th Grade'},
+        6: {'ages': '10-11', 'grade_level': '5th Grade'},
+        7: {'ages': '11-12', 'grade_level': '6th Grade'},
+        8: {'ages': '12-13', 'grade_level': '7th Grade'},
+        9: {'ages': '13-14', 'grade_level': '8th Grade'},
+        10: {'ages': '14-15', 'grade_level': '9th Grade'},
+        11: {'ages': '15-16', 'grade_level': '10th Grade'},
+        12: {'ages': '16-17', 'grade_level': '11th Grade'},
+        13: {'ages': '17-18', 'grade_level': '12th Grade'},
+        14: {'ages': '18-22', 'grade_level': 'College'}
+    }
+
+    score_data = ari_scale.get(score, ari_scale[14])
+
+    chalk.magenta(f"""
+    --------------------------------------------------------
+
+    The ARI for the file, {text_file}, is {score}.
+    This corresponds to a {score_data['grade_level']} level of difficulty
+    that is suitable for an average person {score_data['ages']} years old.
+
+    --------------------------------------------------------
+    
+    """)
+
+
+
+
+def calculate(text: str) -> int:
+    """
+    Calculate the ari score.
+    
+    :param text: 
+    :return: 
+    """
+
+    sents = sents_counter(text)
+    words = word_counter(text)
+    chars = char_counter(text)
+
+    raw_score = 4.71*(chars/words) + .5*(words/sents) - 21.43
+    score = math.ceil(raw_score)
+
+    return score
+
 
 
 def pull_text(book_path: str) -> str:
@@ -40,7 +97,8 @@ def select_book():
     
     :return: int, str
     """
-    books = os.listdir(BOOKS)
+    books = [book for book in os.listdir(BOOKS) if '.txt' in book]
+
     book_menu = """To compute the automated readability index, 
 pick from one of the files below:\n"""
 
@@ -49,8 +107,7 @@ pick from one of the files below:\n"""
     chalk.yellow(book_menu)
 
     for index, book in options.items():
-        if '.txt' in book:
-            print(index, book)
+        print(index, book)
 
 
     while True:
@@ -67,12 +124,16 @@ pick from one of the files below:\n"""
 
             chalk.red("Please enter the number to the left of the book.")
 
-    else:
-        book_title = options[book_choice]
-        full_path = BOOKS + book_title
+        else:
+            book_title = options[book_choice]
+            full_path = BOOKS + book_title
 
-        pull_text(full_path)      # not working here
+            text = pull_text(full_path)
+            score = calculate(text)
+            score_ari(score=score, text_file=book_title)
 
+
+            # resul = ari_results     # calls the function from quantify_text.py
 
 select_book()
 
