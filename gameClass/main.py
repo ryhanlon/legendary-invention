5150c24dafc0bc51ab9bafc19c8b9d3d506c2119
Rebecca Hanlon <ryhanlon@gmail.com>
2017-05-13 23:06:11 -0700

    "Building a class for a memory game"

---
+++ gameClass/main.py
@@ -0,0 +1,33 @@
+"""
+This file was written by Rebecca Hanlon.
+The object is to make a deck of flashcards with duplicates of each item.  
+
+"""
+
+import random
+
+
+class Matching:
+
+    def __init__(self, name: str, deck=2):
+        self.name = name
+
+        fl_cards = ['cat', 'dog', 'bird', 'lizard', 'snake', 'hippo', 'fish', 'horse', 'elephant', 'camel']
+
+
+        vocab
+
+    def __repr__(self):
+        message = "Matching({name}"
+        return message
+
+    def __str__(self):
+        message = "A deck of matching {name} activity."
+        return message
+
+    def flashcards(self, vocabulary: list) -> list:
+        fl_cards = list(vocabulary)
+        return fl_cards
+
+    def shuffle(selfs, ):
+
